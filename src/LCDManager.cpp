5d1781724f3dc04675a36f11744ca5b3036478bd
Hossein Hosni <hosni.hossein@gmail.com>
2024-08-12 00:56:11 +0330

    Show Date, Time, Light, Temp and WiFi info on LCD

--- src/LCDManager.cpp
+++ src/LCDManager.cpp
@@ -5,6 +5,7 @@
 #include <SensorsManager.hpp>
 #include <WifiManager.hpp>
 #include <TimeLib.h>
+#include <DateConvL.h>
 
 extern AsyncTimer asyncTimer;
 
@@ -33,78 +34,52 @@ void _LCDManager::setup()
 	createChar(CELSIUS_SYM, CHAR_CELSIUS);
 	LOG__DEBUG(F("Begin LCDManager::setup [done]"));
 
-	asyncTimer.setInterval([&]() mutable {
-		this->clearRow(0);
+	asyncTimer.setInterval([&]() {
+		const uint8_t dateAndTimeRow = 0;
+		this->setCursor(0, dateAndTimeRow);
+		DateConvL dateC;
+		dateC.ToShamsi(year(), month(), day());
+		this->printf("%04d/%02d/%02d  %02d:%02d:%02d", dateC.global_year, dateC.global_month, dateC.global_day, hour(), minute(), second());
 
-		int lastLength = 0;
-
-		String light(SensorsManager.getLightPercent() + '%');
-		this->setCursor(this->getLcdCols() - (lastLength += light.length()), 0);
-		this->print(light.c_str());
-
-		String temperature((int)SensorsManager.getAvgTempreture());
-		this->setCursor(this->getLcdCols() - (lastLength += temperature.length()
-			+ 1 // one for CELSIUS_SYM
-			+ 1 // one space for space between light percent and temperature
-		), 0);
-		this->print(temperature);
-		this->write(CELSIUS_SYM);
-
-		if (this->getLcdCols() - lastLength >= 5) { // 03:30
-			this->setCursor(0, 0);
-			if (this->getLcdCols() - lastLength >= 8) { // 03:30:33]
-				char time[9];
-				sprintf(time, "%02d:%02d:%02d", hour(), minute(), second());
-				LOG__TRACE_F("time is: %s", time);
-				this->print(time);
-			} else { // 03:30
-				char time[6];
-				sprintf(time, "%02d:%02d", hour(), minute());
-				LOG__TRACE_F("time is: %s", time);
-				this->print(time);
-			}
-		}
+		const uint8_t lightAndTempRow = 1;
+		this->setCursor(0, lightAndTempRow);
+		this->printf("Light %02d%% Temp %.2fc", SensorsManager.getLightPercent(), SensorsManager.getAvgTempreture());
 	}, 1 * 1000);
 
 	int view = 0;
 	asyncTimer.setInterval([&, view]() mutable {
-		this->clearRow(1);
+		const uint8_t line = 2;
+		this->clearRow(line);
 		if (view == 0)
 		{
-			this->setCursor(0, 1);
+			this->setCursor(0, line);
 			this->write(WIFI_SIGNAL_C_SYM);
 
 			String ip = WifiManager.getIpAddress().toString();
-			if (ip.length() < this->getLcdCols() - 1) {
-				this->setCursor(2, 1);
+			if ((unsigned)ip.length() < this->getLcdCols() - 1) {
+				this->setCursor(2, line);
 			} else {
-				this->setCursor(1, 1);
+				this->setCursor(1, line);
 			}
 			this->print(WifiManager.getIpAddress().toString().c_str());
 		}
 		else if (view == 1)
 		{
-			this->setCursor(0, 1);
+			this->setCursor(0, line);
 			this->write(WIFI_SIGNAL_C_SYM);
 			String ssid = WifiManager.getWifiSSID();
-			if (ssid.length() < this->getLcdCols() - 1) {
-				this->setCursor(2, 1);
+			if ((unsigned)ssid.length() < this->getLcdCols() - 1) {
+				this->setCursor(2, line);
 			} else {
-				this->setCursor(1, 1);
+				this->setCursor(1, line);
 			}
-			if (ssid.length() > this->getLcdCols() - 1) {
+			if ((unsigned)ssid.length() > this->getLcdCols() - 1) {
 				ssid = ssid.substring(0, this->getLcdCols() - 1);
 			}
 			this->print(ssid);
 		}
-		else if (view == 2)
-		{
-			this->setCursor(0, 1);
-			this->print(F("FreeHeap: "));
-			this->print(ESP.getFreeHeap());
-		}
 
-		if (++view > 2)
+		if (++view > 1)
 		{
 			view = 0;
 		}
